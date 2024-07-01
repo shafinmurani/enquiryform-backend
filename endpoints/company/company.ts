48e6e8946ac6502163d871b0bfdabb814a855721
Shafin Murani <shafinmurani9@gmail.com>
2024-07-01 16:50:53 +0530

    Company master completed

--- endpoints/company/company.ts
+++ endpoints/company/company.ts
@@ -70,4 +70,52 @@ router.post("/delete", (req, res) => {
   });
 });
 
+router.post("/get-specific", (req, res) => {
+  console.log(req.body.id);
+  const con = new modules.SqlConnection().getConnection();
+  con.connect(function (err) {
+    if (err) throw err;
+    con.query(
+      "SELECT * FROM tbl_company WHERE iCompanyID = ?;",
+      [req.body.id],
+      function (err, result, fields) {
+        if (err) throw err;
+        if (result) {
+          console.log(result);
+          res.json({
+            result: true,
+            list: result,
+          });
+        } else {
+          res.json({ result: false, message: err });
+        }
+      },
+    );
+  });
+});
+
+router.post("/edit", (req, res) => {
+  console.log(req.body.id);
+  const con = new modules.SqlConnection().getConnection();
+  con.connect(function (err) {
+    if (err) throw err;
+    con.query(
+      "UPDATE tbl_company SET vCompanyName = ? WHERE iCompanyID = ?;",
+      [req.body.category, req.body.id],
+      function (err, result, fields) {
+        if (err) throw err;
+        if (result) {
+          res.json({
+            result: true,
+            message: "Category updated successfully",
+            list: result,
+          });
+        } else {
+          res.json({ result: false, message: err });
+        }
+      },
+    );
+  });
+});
+
 export { router as companyRouter };
